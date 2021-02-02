3c091f1db452b755b1ba999d4ad527f9676e826b
DJBLANCOH <djblancoh@gmail.com>
2021-02-01 21:01:33 -0500

    init projecto

---
+++ backend/platzi-ecomerce/src/main/java/com/platzi/ecomerce/HolaMundoController.java
@@ -0,0 +1,4 @@
+package com.platzi.ecomerce;
+
+public class HolaMundoController {
+}
---
+++ backend/platzi-ecomerce/src/test/java/com/platzi/ecomerce/PlatziEcomerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.ecomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziEcomerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
